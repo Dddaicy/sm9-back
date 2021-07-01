6d8361bb19f6ae0a9ca034c1b21a417d427c6cdc
oyf <2368732089@qq.com>
2021-07-01 18:02:48 +0800

    终端侧设备管理dao层

--- src/main/java/com/sm9/boot/dao/TerminalDeviceDao.java
+++ src/main/java/com/sm9/boot/dao/TerminalDeviceDao.java
@@ -2,18 +2,20 @@ package com.sm9.boot.dao;
 
 
 import com.sm9.boot.pojo.TerminalDevice;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface TerminalDeviceDao {
 
     List<TerminalDevice> getTerminalDeviceList(int offset, int count);
     int getTerminalDeviceCount();
-    TerminalDevice getTerminalDeviceById(String centerDeviceId);
-    void addTerminalDevice(TerminalDevice centerDevice);
-    int updateTerminalDevice(TerminalDevice centerDevice);
-    int deleteTerminalDeviceById(String centerDeviceId);
-    List<TerminalDevice> getTerminalDeviceListByCenterDeviceId(String centerDeviceState);
-    int deleteTerminalDeviceByCenterDeviceId(String centerDeviceState);
+    TerminalDevice getTerminalDeviceById(String terminalDeviceId);
+    void addTerminalDevice(TerminalDevice terminalDevice);
+    int updateTerminalDevice(TerminalDevice terminalDevice);
+    int deleteTerminalDeviceById(String terminalDeviceId);
+    List<TerminalDevice> getTerminalDeviceListByCenterDeviceId(String centerDeviceId);
+    int deleteTerminalDeviceByCenterDeviceId(String centerDeviceId);
 
 }
