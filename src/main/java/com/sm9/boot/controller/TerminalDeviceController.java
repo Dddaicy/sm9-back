f8a13a6a7f83d871417cf794f914066bec134c33
oyf <2368732089@qq.com>
2021-07-01 18:02:28 +0800

    终端侧设备管理controller层

--- src/main/java/com/sm9/boot/controller/TerminalDeviceController.java
+++ src/main/java/com/sm9/boot/controller/TerminalDeviceController.java
@@ -2,7 +2,6 @@ package com.sm9.boot.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.sm9.boot.pojo.CenterDevice;
 import com.sm9.boot.pojo.TerminalDevice;
 import com.sm9.boot.service.TerminalDeviceService;
 import com.sm9.boot.util.JsonUtils;
