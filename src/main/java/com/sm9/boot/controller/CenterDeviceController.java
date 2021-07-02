a8bf397e1851e681b963adef1b6580073b2eb03d
oyf <2368732089@qq.com>
2021-07-02 22:56:51 +0800

    中心侧设备管理controller层

--- src/main/java/com/sm9/boot/controller/CenterDeviceController.java
+++ src/main/java/com/sm9/boot/controller/CenterDeviceController.java
@@ -2,13 +2,15 @@ package com.sm9.boot.controller;
 
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.sm9.boot.config.exception.CommonJsonException;
 import com.sm9.boot.pojo.CenterDevice;
 import com.sm9.boot.service.CenterDeviceService;
+import com.sm9.boot.util.ErrorEnum;
 import com.sm9.boot.util.JsonUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 @RestController
@@ -23,40 +25,42 @@ public class CenterDeviceController {
     /**
      * 查询参数中传入id，则按id查询并返回单个设备信息
      * 查询参数中传入center-device-state，则按center-device-state查询并返回设备列表
-     * 前端pageRow传值null时，返回所有设备，传pageNum和pageRow时返回特定页数的设备列表
+     * 传pageNum和pageRow时返回特定页数的设备列表
      *
      * @param json 前端RequestBody数据
      * @param centerDeviceId 中心侧设备Id
      * @param centerDeviceState 中心侧设备状态
-     * @return JSONObject
+     * @return 根据不同的传入参数返回不同的设备（列表）
      */
     @GetMapping(value = "/center-devices")
     public Object getCenterDeviceList(@RequestBody(required = false) JSONObject json,
                                       @RequestParam(value = "id", required = false) String centerDeviceId,
-                                      @RequestParam(value = "center-device-state", required = false) String centerDeviceState) {
+                                      @RequestParam(value = "state", required = false) String centerDeviceState) {
         List<CenterDevice> deviceList;
         int pageNum = 1;
-        Integer pageRow = 10;
-        if(json != null){
-            pageNum = (int) json.getOrDefault("pageNum", 1);
-            pageRow = (Integer) json.getOrDefault("pageRow", 10);
+        int pageRow = 10;
+        try {
+            if(json != null){
+                pageNum = (int) json.getOrDefault("pageNum", 1);
+                pageRow = (int) json.getOrDefault("pageRow", 10);
+            }
+        } catch (ClassCastException e){
+            throw new CommonJsonException(e, "类型转换失败，请传入int类型参数");
         }
         int count = centerDeviceService.getCenterDeviceCount();
         if(centerDeviceId != null){
-            return centerDeviceService.getCenterDeviceById(centerDeviceId);
+            return JsonUtils.successJson(centerDeviceService.getCenterDeviceById(centerDeviceId));
         }else if(centerDeviceState != null){
-            deviceList = centerDeviceService.getCenterDeviceListByState(centerDeviceState);
-        }else if(pageRow == null){
-            deviceList = centerDeviceService.listCenterDevice(0, count);
-        } else
+            deviceList = centerDeviceService.getCenterDeviceListByState(centerDeviceState, (pageNum - 1) * pageRow, pageRow);
+        }else
             deviceList = centerDeviceService.listCenterDevice((pageNum - 1) * pageRow, pageRow);
         return JsonUtils.successPage(JSON.toJSON(deviceList), pageNum, pageRow, count);
     }
 
     @PostMapping("/center-devices")
-    public CenterDevice addCenterDevice(CenterDevice centerDevice){
+    public JSONObject addCenterDevice(CenterDevice centerDevice){
         centerDeviceService.addCenterDevice(centerDevice);
-        return centerDevice;
+        return JsonUtils.successJson(centerDevice);
     }
 
     @DeleteMapping("/center-devices")
@@ -69,16 +73,16 @@ public class CenterDeviceController {
             rows = centerDeviceService.deleteCenterDeviceByState(centerDeviceState);
         else
             rows = 0;
-        return new JSONObject(){{
+        return JsonUtils.successJson(new JSONObject(){{
             put("affectedRows", rows);
-        }};
+        }});
     }
 
     @PutMapping("/center-devices")
     public JSONObject updateCenterDevice(CenterDevice centerDevice){
         int rows = centerDeviceService.updateCenterDevice(centerDevice);
-        return new JSONObject(){{
+        return JsonUtils.successJson(new JSONObject(){{
             put("affectedRows", rows);
-        }};
+        }});
     }
 }
