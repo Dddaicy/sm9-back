84990b341d11c0e49d32980574290084c4e1400a
oyf <2368732089@qq.com>
2021-07-02 22:54:11 +0800

    启动类

--- src/main/java/com/sm9/boot/BootApplication.java
+++ src/main/java/com/sm9/boot/BootApplication.java
@@ -3,9 +3,11 @@ package com.sm9.boot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.sm9.boot.dao")
+@EnableTransactionManagement
 public class BootApplication {
 
     public static void main(String[] args) {
