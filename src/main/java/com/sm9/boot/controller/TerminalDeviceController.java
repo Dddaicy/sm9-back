22adc8690618b0a5af29f9be5db48ad307d4c3a0
oyf <2368732089@qq.com>
2021-07-02 22:57:10 +0800

    终端侧设备管理controller层

--- src/main/java/com/sm9/boot/controller/TerminalDeviceController.java
+++ src/main/java/com/sm9/boot/controller/TerminalDeviceController.java
@@ -2,6 +2,7 @@ package com.sm9.boot.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.sm9.boot.config.exception.CommonJsonException;
 import com.sm9.boot.pojo.TerminalDevice;
 import com.sm9.boot.service.TerminalDeviceService;
 import com.sm9.boot.util.JsonUtils;
@@ -20,32 +21,34 @@ public class TerminalDeviceController {
 
     /**
      * 查询参数中传入id，则按id查询并返回单个设备信息
-     * 查询参数中传入terminal-device-center-device-id，则按terminal-device-center-device-id查询并返回设备列表
-     * 前端pageRow传值null时，返回所有设备，传pageNum和pageRow时返回特定页数的设备列表
+     * 查询参数中传入center-device-id，则按center-device-id查询并返回设备列表
+     * 传pageNum和pageRow时返回特定页数的设备列表
      *
      * @param json 前端RequestBody数据
      * @param terminalDeviceId 终端侧设备Id
-     * @param terminalDeviceCenterDeviceId 终端侧设备所属的中心侧设备的Id
-     * @return JSONObject
+     * @param CenterDeviceId 终端侧设备所属的中心侧设备的Id
+     * @return 根据不同的传入参数返回不同的设备（列表）
      */
     @GetMapping(value = "/terminal-devices")
     public Object getTerminalDeviceList(@RequestBody(required = false) JSONObject json,
                                       @RequestParam(value = "id", required = false) String terminalDeviceId,
-                                      @RequestParam(value = "terminal-device-center-device-id", required = false) String terminalDeviceCenterDeviceId) {
+                                      @RequestParam(value = "center-device-id", required = false) String CenterDeviceId) {
         List<TerminalDevice> deviceList;
         int pageNum = 1;
-        Integer pageRow = 10;
-        if(json != null){
-            pageNum = (int) json.getOrDefault("pageNum", 1);
-            pageRow = (Integer) json.getOrDefault("pageRow", 10);
+        int pageRow = 10;
+        try {
+            if(json != null){
+                pageNum = (int) json.getOrDefault("pageNum", 1);
+                pageRow = (int) json.getOrDefault("pageRow", 10);
+            }
+        } catch (ClassCastException e){
+            throw new CommonJsonException(e, "类型转换失败，请传入int类型参数");
         }
         int count = terminalDeviceService.getTerminalDeviceCount();
         if(terminalDeviceId != null){
-            return terminalDeviceService.getTerminalDeviceById(terminalDeviceId);
-        }else if(terminalDeviceCenterDeviceId != null){
-            deviceList = terminalDeviceService.getTerminalDeviceListByCenterDeviceId(terminalDeviceCenterDeviceId);
-        }else if(pageRow == null){
-            deviceList = terminalDeviceService.listTerminalDevice(0, count);
+            return JsonUtils.successJson(terminalDeviceService.getTerminalDeviceById(terminalDeviceId));
+        }else if(CenterDeviceId != null){
+            deviceList = terminalDeviceService.getTerminalDeviceListByCenterDeviceId(CenterDeviceId, (pageNum - 1) * pageRow, pageRow);
         } else
             deviceList = terminalDeviceService.listTerminalDevice((pageNum - 1) * pageRow, pageRow);
         return JsonUtils.successPage(JSON.toJSON(deviceList), pageNum, pageRow, count);
@@ -53,30 +56,23 @@ public class TerminalDeviceController {
 
     @DeleteMapping("/terminal-devices")
     public JSONObject deleteTerminalDevice(@RequestParam(value = "id", required = false) String terminalDeviceId,
-                                         @RequestParam(value = "terminal-device-center-device-id", required = false) String centerDeviceId){
-        int rows;
+                                         @RequestParam(value = "center-device-id", required = false) String centerDeviceId){
         if(terminalDeviceId != null)
-            rows = terminalDeviceService.deleteTerminalDeviceById(terminalDeviceId);
+            terminalDeviceService.deleteTerminalDeviceById(terminalDeviceId);
         else if (centerDeviceId != null)
-            rows = terminalDeviceService.deleteTerminalDeviceByCenterDeviceId(centerDeviceId);
-        else
-            rows = 0;
-        return new JSONObject(){{
-            put("affectedRows", rows);
-        }};
+            terminalDeviceService.deleteTerminalDeviceByCenterDeviceId(centerDeviceId);
+        return JsonUtils.successJson(null);
     }
 
     @PostMapping("/terminal-devices")
-    public TerminalDevice addTerminalDevice(TerminalDevice terminalDevice){
+    public JSONObject addTerminalDevice(TerminalDevice terminalDevice){
         terminalDeviceService.addTerminalDevice(terminalDevice);
-        return terminalDevice;
+        return JsonUtils.successJson(terminalDevice);
     }
 
     @PutMapping("/terminal-devices")
     public JSONObject updateCenterDevice(TerminalDevice terminalDevice){
-        int rows = terminalDeviceService.updateTerminalDevice(terminalDevice);
-        return new JSONObject(){{
-            put("affectedRows", rows);
-        }};
+        terminalDeviceService.updateTerminalDevice(terminalDevice);
+        return JsonUtils.successJson(null);
     }
 }
