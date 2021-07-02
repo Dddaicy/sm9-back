f1c3505a713d696e2cb282419d6d1ddc1fb14e4e
oyf <2368732089@qq.com>
2021-07-02 22:58:35 +0800

    String工具类

--- src/main/java/com/sm9/boot/util/StringUtils.java
+++ src/main/java/com/sm9/boot/util/StringUtils.java
@@ -12,4 +12,16 @@ public class StringUtils {
     public static boolean isNullOrEmpty(String[] strings) {
         return strings.length == 0;
     }
+
+    /**
+     * 将字符串的首字母转大写
+     * @param str 需要转换的字符串
+     * @return 首字母大写后的原字符串
+     */
+    public static String capitalize(String str) {
+        // 进行字母的ascii编码前移，效率要高于截取字符串进行转换的操作
+        char[] cs=str.toCharArray();
+        cs[0]-=32;
+        return String.valueOf(cs);
+    }
 }
